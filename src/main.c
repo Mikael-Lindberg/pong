6ee50dd6bed7114efa2615302036c7373e6353a7
Mikael <mikael.lindberg93@gmail.com>
2023-04-16 10:32:36 +0200

    small refactor

--- src/main.c
+++ src/main.c
@@ -2,7 +2,15 @@
 
 struct Player {
     bool is_left_player;
+    int width;
+    int height;
+    int space_from_back;
+    int y_pos;
 };
+
+void DrawPlayer(Player* player) {
+    DrawRectangle(player -> space_from_back, player -> y_pos, player -> width, player -> height, WHITE);
+}
 
 void DrawGrid(int i) {
     DrawRectangle(GetScreenWidth() / 2 - 8, i, 8, 8, RAYWHITE);
@@ -16,13 +24,7 @@ int main() {
     SetTargetFPS(60);
 
     // Unsure about correct size, have to check on the window.
-    int player_width = 12;
-    int player_height = 72;
-    int space_from_back = 32;
-    int start_y_pos = GetScreenHeight() / 2 - player_height / 2;
-
-    int left_y = start_y_pos;
-    int right_y = start_y_pos;
+    int screen_center_y = GetScreenHeight() / 2;
 
     // Scores
     int score_left =  0;
@@ -51,8 +53,8 @@ int main() {
     // Grid separation thingy
     int box_radius = 2;
 
-    Player player_left = { true };
-    Player player_right = { false };
+    Player player_left = { true, 12, 72, 32, screen_center_y - 36 };
+    Player player_right = { false, 12, 72, GetScreenWidth() - 32 - 12, screen_center_y - 36 };
 
     while (!WindowShouldClose()) {
         BeginDrawing();
@@ -62,22 +64,18 @@ int main() {
                 DrawGrid(arr[i]);
             }
 
-            if (IsKeyDown(KEY_W) && left_y >= 0) {
-                left_y -= speed;
-            }
-            if (IsKeyDown(KEY_S) && left_y <= GetScreenHeight() - player_height) {
-                left_y += speed;
+            if (IsKeyDown(KEY_W) && player_left.y_pos >= 0) { player_left.y_pos -= speed; }
+            if (IsKeyDown(KEY_S) && player_left.y_pos <= GetScreenHeight() - player_left.height) { 
+                player_left.y_pos += speed; 
             }
 
-            if (IsKeyDown(KEY_UP) && right_y >= 0) {
-                right_y -= speed;
-            }
-            if (IsKeyDown(KEY_DOWN) && right_y <= GetScreenHeight() - player_height) {
-                right_y += speed;
+            if (IsKeyDown(KEY_UP) && player_right.y_pos >= 0) { player_right.y_pos -= speed; }
+            if (IsKeyDown(KEY_DOWN) && player_right.y_pos <= GetScreenHeight() - player_right.height) { 
+                player_right.y_pos += speed; 
             }
 
-            DrawRectangle(space_from_back, left_y, player_width, player_height, WHITE);
-            DrawRectangle(GetScreenWidth() - (space_from_back + player_width), right_y, player_width, player_height, WHITE);
+            DrawPlayer(&player_left);
+            DrawPlayer(&player_right);
 
             //DrawRectangle(200, 200, ball_size, ball_size, WHITE);
 
