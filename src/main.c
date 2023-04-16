3f514d7d261c30c07693ee19c4fdaabdfd55de82
Mikael <mikael.lindberg93@gmail.com>
2023-04-16 10:48:40 +0200

    refactored grid drawing

--- src/main.c
+++ src/main.c
@@ -12,46 +12,35 @@ void DrawPlayer(Player* player) {
     DrawRectangle(player -> space_from_back, player -> y_pos, player -> width, player -> height, WHITE);
 }
 
-void DrawGrid(int i) {
-    DrawRectangle(GetScreenWidth() / 2 - 8, i, 8, 8, RAYWHITE);
+void DrawGridLine(int size, int spacing) {
+    for (int i = spacing/2; i <= GetScreenHeight(); i += size + spacing) {
+        DrawRectangle(GetScreenWidth() / 2 - size / 2, i, size, size, RAYWHITE);
+    }
 }
 
 int main() {
+    // Screen related stuff
     const int screen_width = 800;
     const int screen_height = 480;
+    const int screen_center_y = screen_height / 2;
+    const int screen_center_x = screen_width / 2;
 
+    // Initialization related calls
     InitWindow(screen_width, screen_height, "Pong!");
     SetTargetFPS(60);
-
-    // Unsure about correct size, have to check on the window.
-    int screen_center_y = GetScreenHeight() / 2;
 
     // Scores
     int score_left =  0;
     int score_right = 0;
     int max_score = 9;
 
+    // Gameplay
     int delay = 1;
     int speed = 4;
 
-    int ball_size = 12;
-
-    //480
-    int arr[60];
-    int i, value;
-
-    value = 8;
-    for (i = 0; i < 60; i++) {
-        arr[i] = value;
-        value += 24;
-    }
-
-    // These two need to be calculated.
-    int left_position = 0;
-    int right_position = 0;
-
-    // Grid separation thingy
-    int box_radius = 2;
+    // Grid in middle
+    int grid_size = 8;
+    int grid_spacing = 22;
 
     Player player_left = { true, 12, 72, 32, screen_center_y - 36 };
     Player player_right = { false, 12, 72, GetScreenWidth() - 32 - 12, screen_center_y - 36 };
@@ -60,9 +49,7 @@ int main() {
         BeginDrawing();
             ClearBackground(BLACK);
 
-            for (i = 0; i < 60; i++) {
-                DrawGrid(arr[i]);
-            }
+            DrawGridLine(grid_size, grid_spacing);
 
             if (IsKeyDown(KEY_W) && player_left.y_pos >= 0) { player_left.y_pos -= speed; }
             if (IsKeyDown(KEY_S) && player_left.y_pos <= GetScreenHeight() - player_left.height) { 
